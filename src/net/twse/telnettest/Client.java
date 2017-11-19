0a854c678d54b809894d7c020c3726ba64d44bc7
anrock1985 <anrock85@gmail.com>
2017-11-19 17:42:52 +0700

    Initial commit

---
+++ src/net/twse/telnettest/Client.java
@@ -0,0 +1,27 @@
+package net.twse.telnettest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+    private final int TELNET_PORT = 23;
+    private Socket socket;
+    private final String LOGIN = "a.pokazanov";
+    private final String PASSWORD = "celokoD)";
+
+    Client(String ip) throws IOException {
+        this.socket = new Socket(ip, TELNET_PORT);
+    }
+
+    void login() throws IOException {
+        if (socket.isConnected()) {
+            System.out.println("Socket connected to " + socket.getInetAddress() + ":" + socket.getPort());
+            InputStream sockin = socket.getInputStream();
+            OutputStream sockout = socket.getOutputStream();
+            System.out.println(sockin.read());
+        }
+    }
+}
+
--- src/net/twse/telnettest/Main.java
+++ src/net/twse/telnettest/Main.java
@@ -1,4 +1,10 @@
 package net.twse.telnettest;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Client dlink = new Client("10.54.66.11");
+        dlink.login();
+    }
 }
