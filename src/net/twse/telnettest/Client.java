cd252728afaa038b3ea085008e30dffac2147869
Andrey N Pokazanov <anrock85@gmail.com>
2018-02-16 16:17:25 +0700

    Basic Telnet reader almost done. TODO: Why first byte after LF CR is missing?

--- src/net/twse/telnettest/Client.java
+++ src/net/twse/telnettest/Client.java
@@ -1,29 +1,64 @@
 package net.twse.telnettest;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
-    private final int TELNET_PORT = 23;
     private Socket socket;
-    private final String LOGIN = "a.pokazanov";
-    private final String PASSWORD = "celokoD)";
+//    final String LOGIN = "admin";
+//    final String PASSWORD = "dEsKtOpF1685";
 
     Client(String ip) throws IOException {
+//        final int TELNET_PORT = 22;
+        final int TELNET_PORT = 23;
+//        final int TELNET_PORT = 80;
         this.socket = new Socket(ip, TELNET_PORT);
     }
 
     void login() throws IOException {
+        final int lfCmd = 10;
+        final int crCmd = 13;
+        boolean rawBytes = false;
+        boolean reading = true;
+
         if (socket.isConnected()) {
-            System.out.println("Socket connected to " + socket.getInetAddress() + ":" + socket.getPort());
-            InputStream sockin = socket.getInputStream();
-            OutputStream sockout = socket.getOutputStream();
-            while (sockin.read() != -1)
-                System.out.println(sockin.read());
-            sockin.close();
-            sockout.close();
+            BufferedInputStream sockIn = new BufferedInputStream(socket.getInputStream());
+//            BufferedOutputStream sockOut = new BufferedOutputStream(socket.getOutputStream());
+
+            while (reading) {
+                if (sockIn.read() != -1) {
+                    int inputByte;
+                    System.out.println(sockIn.available() + " bytes available");
+                    while (sockIn.available() > 0) {
+                        inputByte = sockIn.read();
+                        if (!rawBytes) {
+                            switch (inputByte) {
+                                case lfCmd:
+                                    sockIn.read();
+                                    System.out.println("LF CR");
+                                    break;
+                                case crCmd:
+                                    System.out.println("CR");
+                                    break;
+                                default:
+//                                    System.out.print(inputByte);
+//                                    if (sockIn.available() != 0)
+//                                        System.out.print(" ");
+                                    System.out.print((char) inputByte);
+                            }
+                        } else {
+                            System.out.print(inputByte);
+                            if (sockIn.available() != 0)
+                                System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
+                } else {
+                    reading = false;
+                }
+            }
+            sockIn.close();
+            System.out.println("Socket closed!");
         }
     }
 }
--- src/net/twse/telnettest/Main.java
+++ src/net/twse/telnettest/Main.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Client dlink = new Client("10.54.66.11");
+        Client dlink = new Client("192.168.1.1");
+//        Client dlink = new Client("54.201.255.163");
+//        Client dlink = new Client("82.200.18.194");
+//        Client dlink = new Client("35.160.169.47");
         dlink.login();
     }
 }
