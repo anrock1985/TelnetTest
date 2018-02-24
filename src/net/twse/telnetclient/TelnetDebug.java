3e37f71cccc6e65663c6320d514f97e0ec4aa153
anrock1985 <anrock85@gmail.com>
2018-02-24 23:38:27 +0700

    1

--- src/net/twse/telnetclient/TelnetDebug.java
+++ src/net/twse/telnetclient/TelnetDebug.java
@@ -3,17 +3,17 @@ package net.twse.telnetclient;
 import java.io.*;
 
 public class TelnetDebug {
-    private FileWriter out;
+    private PrintWriter out;
 
     TelnetDebug(String pathToLogFile) {
         try {
             File logFile = new File(pathToLogFile);
-            out = new FileWriter(logFile, true);
+            out = new PrintWriter(new FileWriter(logFile, true));
             if (!logFile.exists()) {
                 if (!logFile.createNewFile())
                     System.out.println("Cannot create log file!");
             }
-            out.write("\n\nLog file updated\n");
+            out.print("\n\nLog file updated\n");
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
         } catch (IOException e) {
@@ -22,10 +22,6 @@ public class TelnetDebug {
     }
 
     void log(String s) {
-        try {
             out.write(s);
-        } catch (IOException e) {
-            System.out.println("Cannot write to file!");
-        }
     }
 }
